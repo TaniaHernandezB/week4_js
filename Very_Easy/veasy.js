069db4faecd45249eebd2f327764c65ac90d4fdf
Tania Hernandez <taniahernandez@Tanias-MacBook-Air.local>
2022-07-06 16:49:31 -0400

    very easy

---
+++ Very_Easy/veasy.js
@@ -0,0 +1,10 @@
+// VERY EASY: Write a function named min that takes two arguments and returns their minimum.
+
+function min(num1, num2){
+    if (num1<num2){
+        return num1
+    }else{
+        return num2
+    }
+}
+console.log(min(9, 6))
